ce5a187e4c3e9163e6359ce56d9a928d0552d7a6
JSextonn <jsexton11295@gmail.com>
2018-10-22 14:20:19 -0500

    Sources now support many to many relationship with users.

--- CashFlux.Data/CashFluxDbContext.cs
+++ CashFlux.Data/CashFluxDbContext.cs
@@ -11,5 +11,16 @@ namespace CashFlux.Data
 		public DbSet<FluxProfile> Profiles { get; set; }
 		public DbSet<Flux> Fluxes { get; set; }
 		public DbSet<FluxSource> Sources { get; set; }
+		public DbSet<UserSources> UserSources { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+			// Allows composite key to be primary key for user and source junction table.
+			builder.Entity<UserSources>().HasKey(table => new
+			{
+				table.UserId, table.SourceId
+			});
+		}
 	}
 }
