9c7a93697120209f5b6e3621eaa4907867c91569
JSextonn <jsexton11295@gmail.com>
2018-10-24 20:35:01 -0500

    Model now includes username

--- CashFlux.Web/Features/User/UserGetRequestModel.cs
+++ CashFlux.Web/Features/User/UserGetRequestModel.cs
@@ -9,6 +9,7 @@ namespace CashFlux.Web.Features.User
 	public class UserGetRequestModel
 	{
 		public string Id { get; set; }
+		public string Username { get; set; }
 		public string Email { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
