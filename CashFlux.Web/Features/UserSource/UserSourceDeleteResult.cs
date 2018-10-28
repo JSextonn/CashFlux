5cb5bfeb1faa03a63e3462e88d7a8df021c9d572
JSextonn <jsexton11295@gmail.com>
2018-10-27 23:05:40 -0500

    Added delete functionality to UserSource feature

--- CashFlux.Web/Features/UserSource/UserSourceDeleteResult.cs
+++ CashFlux.Web/Features/UserSource/UserSourceDeleteResult.cs
@@ -2,6 +2,7 @@ namespace CashFlux.Web.Features.UserSource
 {
 	public class UserSourceDeleteResult
 	{
-		
+		public string UserDeleteFrom { get; set; }
+		public string SourceDeletedFromUser { get; set; }
 	}
 }
