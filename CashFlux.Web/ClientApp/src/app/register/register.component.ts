89e50a0206bae2560a9ad241c032b6b9de22a3d5
JSextonn <jsexton11295@gmail.com>
2018-10-29 01:27:27 -0500

    Fixed validation functionality

--- CashFlux.Web/ClientApp/src/app/register/register.component.ts
+++ CashFlux.Web/ClientApp/src/app/register/register.component.ts
@@ -1,31 +1,69 @@
-import { Component, OnInit } from '@angular/core';
-import { AbstractControl, Validators, FormControl, FormGroup, ValidatorFn } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subject } from "rxjs";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
-  registerForm : FormGroup;
+export class RegisterComponent implements OnInit, OnDestroy {
+  private componentDestroyed: Subject<void> = new Subject();
 
+  email: FormControl;
+  password: FormControl;
+  confirm: FormControl;
+  first: FormControl;
+  last: FormControl;
+  registerForm: FormGroup;
+
+  // TODO: Inject user service for account creation
   constructor() { }
 
-  ngOnInit() { 
-    // Create FormGroup with FormControls as children
-    this.registerForm = new FormGroup({
-      'email': new FormControl('', [Validators.email, Validators.required]),
-      'password': new FormControl('', Validators.required),
-      'confirmPassword': new FormControl('', Validators.required),
-      'firstName': new FormControl('', Validators.required),
-      'lastName': new FormControl('', Validators.required),
-    });
+  ngOnInit() {
+    this.buildFormControls();
+    this.buildFromGroup();
   }
 
-  // passwordMatchValidator(form: FormGroup): ValidatorFn {
-  //   return (control: AbstractControl): { [key: string]: any } | null => {
-  //     return form.get('password').value === form.get('confirmPassword').value
-  //       ? { 'mismatch': { value: true } } : null;
-  //   };
-  // }
+  ngOnDestroy(): void {
+    this.componentDestroyed.next();
+    this.componentDestroyed.unsubscribe();
+  }
+
+  submit() {
+    if (!this.registerForm.valid) {
+      // TODO: Make sure the invalid control is in error state
+    }
+
+    // TODO: Submit form values with injected user service
+  }
+
+  private matchesPasswordValidator(control: AbstractControl): { [key: string]: boolean } {
+    return control.value !== this.password.value ?
+      {'mismatch': true} : null;
+  }
+
+  private buildFormControls(): void {
+    this.email = new FormControl('', [Validators.email, Validators.required]);
+    this.password = new FormControl('', Validators.required);
+    this.confirm = new FormControl('', [Validators.required, this.matchesPasswordValidator.bind(this)]);
+    this.first = new FormControl('', [Validators.minLength(3), Validators.maxLength(25)]);
+    this.last = new FormControl('', [Validators.minLength(3), Validators.maxLength(25)]);
+
+    // Make sure the confirmation field always updates with the password field.
+    this.password.valueChanges
+      .subscribe(() => {
+        this.confirm.updateValueAndValidity();
+      });
+  }
+
+  private buildFromGroup(): void {
+    this.registerForm = new FormGroup({
+      email: this.email,
+      password: this.password,
+      confirm: this.confirm,
+      first: this.first,
+      last: this.last
+    });
+  }
 }
