dca58e9a78a1d9e5063fa96bd80319b38c5682ba
JSextonn <jsexton11295@gmail.com>
2018-11-01 21:18:46 -0500

    Now loads previous login info if it exists. Now redirects users to login screen when trying to access dashboard unauthenticated.

--- CashFlux.Web/ClientApp/src/app/app.component.ts
+++ CashFlux.Web/ClientApp/src/app/app.component.ts
@@ -1,156 +1,56 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { AppState } from './app.state';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs';
-import { AddProfiles } from "./redux/actions/profile.actions";
-import { AddFluxes } from "./redux/actions/flux.actions";
-import { AddSources } from "./redux/actions/source.actions";
-import { selectSourceIds } from "./redux/reducers/source.reducer";
-import { selectProfileIds } from "./redux/reducers/profile.reducer";
 import { NavigationStart, Router } from "@angular/router";
 import { filter } from "rxjs/operators";
-import { AuthenticationService } from "./services/authentication.service";
+import { AppState } from "./redux/app.state";
+import { Authentication, selectAuthentication } from "./redux/reducers/auth.reducer";
+import { Observable } from "rxjs";
+import { LoadPreviousLogin } from "./redux/actions/auth.actions";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  title = 'Cash Flux';
+export class AppComponent implements OnInit {
+  private authenticationState: Observable<Authentication>;
+  private loggedIn: boolean = false;
 
-  private profileIdsSubscription: Subscription;
-  private profileIds: string[] = [];
+  constructor(private store: Store<AppState>,
+              private router: Router) {
+    this.authenticationState = this.store.select(selectAuthentication);
+  }
 
-  private sourceIdsSubscription: Subscription;
-  private sourceIds: string[] = [];
+  ngOnInit() {
+    this.loadLocalLoginInfo();
+    this.handleLoginStatus();
+    this.redirectToLogin();
+  }
 
-  constructor(private _store: Store<AppState>,
-              private router: Router,
-              private authService: AuthenticationService) {
-    router.events
+  private handleLoginStatus() {
+    this.authenticationState
+      .subscribe((data: Authentication) => {
+        this.loggedIn = data.loggedIn;
+      });
+  }
+
+  private loadLocalLoginInfo() {
+    const loginInfo = localStorage.getItem('login');
+
+    if (loginInfo) {
+      this.store.dispatch(new LoadPreviousLogin(JSON.parse(loginInfo)));
+    }
+  }
+
+  private redirectToLogin() {
+    this.router.events
       .pipe(filter(event => event instanceof NavigationStart))
+      // Only collect routes trying to reach the dashboard
       .pipe(filter((event: NavigationStart) => event.url === '/dashboard'))
       .subscribe((event: NavigationStart) => {
-        // Redirect users to login if they're not logged in trying to access dashboard.
-        if (!this.authService.loggedIn.value) {
-          this.router.navigate(['/login']);
+        if (!this.loggedIn) {
+          this.router.navigateByUrl('/login');
         }
-      })
-  }
-
-  ngOnInit(): void {
-    this.setupSubscriptions();
-
-    // Add dummy profiles
-    this._store.dispatch(new AddProfiles([{
-      name: 'Test One',
-      timeCreated: new Date()
-    }, {
-      name: 'Test Two',
-      timeCreated: new Date()
-    }]));
-
-    //     Add dummy sources
-    this._store.dispatch(new AddSources([{
-      name: 'Wal-mart',
-      category: 'Food',
-      timeCreated: new Date()
-    }, {
-      name: 'Exxon',
-      category: 'Gas',
-      timeCreated: new Date()
-    }]));
-
-    this._store.dispatch(new AddFluxes([
-      {
-        amount: -100,
-        sourceId: this.sourceIds[0],
-        profileId: this.profileIds[0],
-        timeCreated: new Date()
-      },
-      {
-        amount: -100,
-        sourceId: this.sourceIds[0],
-        profileId: this.profileIds[0],
-        timeCreated: new Date()
-      },
-      {
-        amount: -100,
-        sourceId: this.sourceIds[0],
-        profileId: this.profileIds[0],
-        timeCreated: new Date()
-      },
-      {
-        amount: -90,
-        sourceId: this.sourceIds[0],
-        profileId: this.profileIds[0],
-        timeCreated: new Date()
-      },
-      {
-        amount: -20,
-        sourceId: this.sourceIds[0],
-        profileId: this.profileIds[0],
-        timeCreated: new Date()
-      },
-      {
-        amount: -19,
-        sourceId: this.sourceIds[0],
-        profileId: this.profileIds[0],
-        timeCreated: new Date()
-      },
-      {
-        amount: -17,
-        sourceId: this.sourceIds[0],
-        profileId: this.profileIds[0],
-        timeCreated: new Date()
-      },
-      {
-        amount: -10,
-        sourceId: this.sourceIds[0],
-        profileId: this.profileIds[0],
-        timeCreated: new Date()
-      }, {
-        amount: -5,
-        sourceId: this.sourceIds[0],
-        profileId: this.profileIds[0],
-        timeCreated: new Date()
-      }, {
-        amount: -10,
-        sourceId: this.sourceIds[0],
-        profileId: this.profileIds[0],
-        timeCreated: new Date()
-      }, {
-        amount: -1,
-        sourceId: this.sourceIds[0],
-        profileId: this.profileIds[0],
-        timeCreated: new Date()
-      }, {
-        amount: -50,
-        sourceId: this.sourceIds[0],
-        profileId: this.profileIds[0],
-        timeCreated: new Date()
-      }, {
-        amount: -100,
-        sourceId: this.sourceIds[0],
-        profileId: this.profileIds[0],
-        timeCreated: new Date()
-      }
-    ]));
-  }
-
-  ngOnDestroy(): void {
-    this.profileIdsSubscription.unsubscribe();
-    this.sourceIdsSubscription.unsubscribe();
-  }
-
-  private setupSubscriptions(): void {
-    this.sourceIdsSubscription = this._store.select(selectSourceIds).subscribe(data => {
-      this.sourceIds = data as string[];
-    });
-
-    this.profileIdsSubscription = this._store.select(selectProfileIds).subscribe(data => {
-      this.profileIds = data as string[];
-    });
+      });
   }
 }
