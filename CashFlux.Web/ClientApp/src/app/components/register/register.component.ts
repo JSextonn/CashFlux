c575b5dfb818deb3e28413437bb0f61bab870e24
JSextonn <jsexton11295@gmail.com>
2018-11-02 09:09:53 -0500

    Register page now enforces new password policy

--- CashFlux.Web/ClientApp/src/app/components/register/register.component.ts
+++ CashFlux.Web/ClientApp/src/app/components/register/register.component.ts
@@ -8,6 +8,7 @@ import { Subject } from "rxjs";
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit, OnDestroy {
+  private readonly PASSWORD_PATTERN = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/g;
   private componentDestroyed: Subject<void> = new Subject();
 
   email: FormControl;
@@ -43,9 +44,14 @@ export class RegisterComponent implements OnInit, OnDestroy {
       {'mismatch': true} : null;
   }
 
+  private meetsPasswordPolicy(control: AbstractControl): { [key: string]: boolean } {
+    return this.PASSWORD_PATTERN.test(control.value) ?
+      null : {'meetspolicy': true};
+  }
+
   private buildFormControls(): void {
     this.email = new FormControl('', [Validators.email, Validators.required]);
-    this.password = new FormControl('', Validators.required);
+    this.password = new FormControl('', [Validators.required, this.meetsPasswordPolicy.bind(this)]);
     this.confirm = new FormControl('', [Validators.required, this.matchesPasswordValidator.bind(this)]);
     this.first = new FormControl('', [Validators.minLength(3), Validators.maxLength(25)]);
     this.last = new FormControl('', [Validators.minLength(3), Validators.maxLength(25)]);
